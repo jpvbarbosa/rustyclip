4994a652b685a194923b7160aaf39d234994d44f
JP Barbosa <jpvbarbosa@outlook.com>
2023-02-24 11:22:47 -0800

    Prototype that reads TOML config file and stores a list of user input as a pseudo clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::io;
+use std::fs;
+use toml::Table;
+
+struct Config{
+    mem_capacity: usize
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let configpath = "Config.toml";
+    
+    let config = fs::read_to_string(configpath).expect("Failed to read config file");
+
+    let config = config.parse::<Table>().expect("Failed to parse config file");
+
+    let mem_capacity = match config["defaults"]["mem_capacity"].as_integer() {
+        Some(v) => v,
+        None => {
+            println!("Failed to read mem_capacity. Using default of 10");
+            10
+        }
+    };
+
+    let config = Config { mem_capacity: mem_capacity as usize};
+
+    let mut clipboard = Vec::<String>::with_capacity(config.mem_capacity);
+
+    main_process(&mut clipboard);
+}
+
+
+fn main_process(clipboard: &mut Vec::<String>)
+{
+    loop {
+        let mut input = String::new();
+        io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to get user input");
+
+        clipboard.push(input.trim().to_string()); // Temporary trim to remove \n from console input
+
+        println!("Clipboard is {:?}", clipboard)
+    }
 }
